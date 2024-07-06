c24ae36ff4e62a485a8d30cc26b0072a8ef7f3e5
radin <dlwlsgml8847@gmail.com>
2024-07-06 18:00:44 +0900

    [Design/#19] 이미지 및 컬러에셋 추가/수정

--- MatQ_Admin/Presentation/Component/ButtonLabelComponent.swift
+++ MatQ_Admin/Presentation/Component/ButtonLabelComponent.swift
@@ -19,7 +19,7 @@ struct ButtonLabelComponent: View {
         var fgColor: Color {
             switch self {
             case .primary:
-                Color.black.opacity(0.8)
+                Color.white
             case .secondary:
                 Color.black.opacity(0.8)
             case .delete:
@@ -30,7 +30,7 @@ struct ButtonLabelComponent: View {
         var bgColor: Color {
             switch self {
             case .primary:
-                Color.main
+                Color.primaryPurple
             case .secondary:
                 Color.gray100
             case .delete:
@@ -38,7 +38,7 @@ struct ButtonLabelComponent: View {
             }
         }
     }
-
+    
     var body: some View {
         Text(title)
             .frame(maxWidth: .infinity)
@@ -48,13 +48,75 @@ struct ButtonLabelComponent: View {
             .background(type.bgColor)
             .cornerRadius(12)
     }
+    
+}
+
+struct IS_ButonView: ButtonStyle {
+    let type: ButtonType
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 16)
+            .foregroundColor(type.fgColor)
+            .font(.headline)
+            .background(type.bgColor)
+            .cornerRadius(12)
+    }
+    
+    
+    enum ButtonType {
+        case primary
+        case secondary
+        case delete
+        
+        var fgColor: Color {
+            switch self {
+            case .primary:
+                Color.white
+            case .secondary:
+                Color.black.opacity(0.8)
+            case .delete:
+                Color.white
+            }
+        }
+        
+        var bgColor: Color {
+            switch self {
+            case .primary:
+                Color.primaryPurple
+            case .secondary:
+                Color.gray100
+            case .delete:
+                Color.mainRed
+            }
+        }
+    }
 }
 
 
 #Preview {
     VStack {
-        ButtonLabelComponent(title: "공지 삭제하기", type: .primary)
-        ButtonLabelComponent(title: "공지 삭제하기", type: .secondary)
-        ButtonLabelComponent(title: "공지 삭제하기", type: .delete)
+        Button {} label : { Text("공지 삭제하기") }.buttonStyle(IS_ButonView(type: .delete))
+        Button(action: {}, label: {
+            Text("Button")
+        })
+        .ilsangButtonStyle(type: .delete)
+        //        IS_ButonView(title: "공지 삭제하기", type: .secondary)
+        //        IS_ButonView(title: "공지 삭제하기", type: .delete)
+    }
+}
+
+struct ISButton: ViewModifier {
+    let type: IS_ButonView.ButtonType
+    
+    func body(content: Content) -> some View {
+        content
+            .buttonStyle(IS_ButonView(type: type))
+    }
+}
+extension Button {
+    func ilsangButtonStyle(type: IS_ButonView.ButtonType) -> some View {
+        modifier(ISButton(type: type))
     }
 }
