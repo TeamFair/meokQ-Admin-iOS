aa212d4cf4657091d677d684301b09aa7378f68c
radin <dlwlsgml8847@gmail.com>
2024-08-19 03:25:55 +0900

    [Feat/#44] QuestService, ImageService를 NetworkUtils 활용하도록 리팩토링

--- MatQ_Admin/Data/Network/Service/QuestService.swift
+++ MatQ_Admin/Data/Network/Service/QuestService.swift
@@ -17,42 +17,16 @@ protocol QuestServiceInterface {
 
 struct QuestService: QuestServiceInterface {
     func getQuestList(request: GetQuestRequest) -> AnyPublisher<[GetQuestResponseData], NetworkError> {
-        AF.request(QuestTarget.getQuest(request))
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type: ResponseWithPage<[GetQuestResponseData]>.self)
-            .tryMap { response in
-                guard let value = response.value else {
-                    throw NetworkError.serverError
-                }
-                return value.data
-            }
-            .mapError { _ in NetworkError.serverError }
+        NetworkUtils.request(QuestTarget.getQuest(request), as: ResponseWithPage<[GetQuestResponseData]>.self)
+            .map { $0.data }
             .eraseToAnyPublisher()
     }
     
     func postQuest(request: PostQuestRequest) -> AnyPublisher<PostQuestResponse, NetworkError> {
-        AF.request(QuestTarget.postQuest(request))
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type: PostQuestResponse.self)
-            .value()
-            .mapError { error in
-                print(error.localizedDescription)
-                return NetworkError.serverError
-            }
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+        NetworkUtils.request(QuestTarget.postQuest(request), as: PostQuestResponse.self)
     }
-    
-    func deleteQuest(request: DeleteQuestRequest) -> AnyPublisher<PostQuestResponse, NetworkError> {
-        AF.request(QuestTarget.deleteQuest(request))
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type: DeleteQuestResponse.self)
-            .value()
-            .mapError { error in
-                print(error.localizedDescription)
-                return NetworkError.serverError
-            }
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+
+    func deleteQuest(request: DeleteQuestRequest) -> AnyPublisher<DeleteQuestResponse, NetworkError> {
+        NetworkUtils.request(QuestTarget.deleteQuest(request), as: DeleteQuestResponse.self)
     }
 }
