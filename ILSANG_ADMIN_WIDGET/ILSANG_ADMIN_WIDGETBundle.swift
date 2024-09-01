70a9ffb67d8164edc1b7bff139ad4791650553b7
radin <dlwlsgml8847@gmail.com>
2024-09-01 18:44:55 +0900

    [Feat/#78] 위젯 타겟 추가 및 기본 작업

---
+++ ILSANG_ADMIN_WIDGET/ILSANG_ADMIN_WIDGETBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ILSANG_ADMIN_WIDGETBundle.swift
+//  ILSANG_ADMIN_WIDGET
+//
+//  Created by Lee Jinhee on 9/1/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ILSANG_ADMIN_WIDGETBundle: WidgetBundle {
+    var body: some Widget {
+        ILSANG_ADMIN_WIDGET()
+    }
+}
